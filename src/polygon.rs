967c3af53a267513b492ac54011b5dba65d896cb
Caleb Dastrup <calebjdastrup@gmail.com>
2023-05-10 08:26:21 -0400

    Removed comments

--- src/polygon.rs
+++ src/polygon.rs
@@ -2,41 +2,7 @@ use std::f32::consts::PI;
 
 //Current shapes package can't do arbitrary polygons?
 use macroquad::prelude::*;
-//Doesn't handle the non-convex case properly -- I need circular linked lists!
-/*fn triangulate_helper(root: Vec2, pts: &[Vec2]) -> Vec<[Vec2; 3]> {
-    if pts.len()==2 {
-        return vec![[root, pts[0],pts[1]]]
-    }
-    let right = pts[0];
-    let left = pts[pts.len()-1];
-    let mut closest: Option<&Vec2> = None;
-    let mut min_dist = INFINITY;
-    let mut index = 0;
-    for (i,p) in pts.iter().enumerate() {
-        let d = *p-root;
-        if d.perp_dot(right-root)>0. && d.perp_dot(root-left)>0. && (*p-right).perp_dot(left-right)<0. {
-            let dist=d.dot(d);
-            if dist<min_dist {
-                closest=Some(p);
-                min_dist=dist;
-                index=i;
-            }
-        }
-    }
-    if closest.is_none(){
-        let mut res = triangulate_helper(right,&pts[1..]);
-        res.push([root, right, left]);
-        return res
-    }
-    let mut first = triangulate_helper(root, &pts[..index+1]);
-    let mut second = triangulate_helper(root, &pts[index..]);
-    first.append(&mut second);
-    first
-}
 
-pub fn triangulate(pts: &[Vec2])->Vec<[Vec2;3]>{
-    triangulate_helper(pts[0], &pts[1..])
-}*/
 fn positive_angle(u: Vec2, v: Vec2) -> f32 {
     let res = u.angle_between(v);
     if res >= 0. {
