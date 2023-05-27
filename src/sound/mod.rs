e3edcca12abafa0278c0986b831e70ad6c4e1926
zbrachinara <zbrachinara@gmail.com>
2023-05-27 15:27:40 -0400

    create endpoint for wasm audio

--- src/sound/mod.rs
+++ src/sound/mod.rs
@@ -1,7 +1,10 @@
 pub mod chord;
 cfg_if::cfg_if! {
-    if #[cfg(target = "wasm32-unknown-unknown")] {
-
+    if #[cfg(target_arch = "wasm32")] {
+        mod native {
+            pub mod signal_js;
+        }
+        pub use native::signal_js as signal;
     } else {
         mod native {
             pub mod signal_rodio;
---
+++ src/sound/native/signal_js.rs
@@ -0,0 +1,11 @@
+use crate::sound::chord::Chord;
+
+
+#[derive(Default)]
+pub struct Updater;
+
+impl Updater {
+    pub fn update(&mut self, chord: Chord) -> Result<(), ()> {
+        todo!()
+    }
+}
