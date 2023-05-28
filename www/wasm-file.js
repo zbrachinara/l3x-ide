24617232eedc4530ea2e90752647a31963f9c1d0
zbrachinara <zbrachinara@gmail.com>
2023-05-28 17:43:00 -0400

    add wasm plugin file

---
+++ www/wasm-file.js
@@ -0,0 +1,6 @@
+
+register_plugin = function (importObject) {
+}
+
+// miniquad_add_plugin receive an object with two fields: register_plugin and on_init. Both are functions, both are optional.
+miniquad_add_plugin({ register_plugin });
